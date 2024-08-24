b849f69f04cbaba03e1ff374f21d75281aa936d5
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2024-08-24 14:14:01 +0200

    Init commit

---
+++ SortItOut/SortItOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortItOutApp.swift
+//  SortItOut
+//
+//  Copyright (c) 2024 Aleksander Stojanowski. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SortItOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SortItOut/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SortItOut
+//
+//  Copyright (c) 2024 Aleksander Stojanowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
